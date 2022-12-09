c2f0687cbd7fdcee564a51db898400956c69b1e5
Pythonikk <nikkibrusoe@gmail.com>
2022-12-09 11:28:32 -0800

    add general info form to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React, { Component } from "react";
+import "./styles/app.css";
 
 class App extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return(
+      <div>
+        <form>
+          <fieldset className="general-info">
+            <legend>General Information</legend>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name"/>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email"/>
+            <label htmlFor="phone">Phone Number</label>
+            <input type="tel"/>
+          </fieldset>
+        </form>
+        {/* // EducationSection here
+        // ExperienceSection here
+        // final submit button */}
+      </div>
+    )
+  }
 
 }
 

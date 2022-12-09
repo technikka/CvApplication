07c61f59453ec32f68597b78e948989cfbd246de
Pythonikk <nikkibrusoe@gmail.com>
2022-12-09 14:48:28 -0800

    add state for education form and handling of submit

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -11,11 +11,19 @@ class EducationSection extends Component {
     };
 
     this.toggleForm = this.toggleForm.bind(this);
+    this.createItem = this.createItem.bind(this);
   }
 
   toggleForm() {
     this.setState({
-      formShowing: !this.state.formShowing
+      formShowing: !this.state.formShowing,
+    });
+  }
+
+  createItem(e, newItem) {
+    e.preventDefault();
+    this.setState({
+      items: this.state.items.concat(newItem),
     });
   }
 
@@ -23,10 +31,10 @@ class EducationSection extends Component {
     return (
       <div>
         <h1>Educational Experience</h1>
-        <button onClick={this.toggleForm}>+/-</button>
-        { this.state.formShowing === true &&
-          <EducationSectionForm />
-        }
+        <button onClick={this.toggleForm}>Hide/Show Form</button>
+        {this.state.formShowing === true && (
+          <EducationSectionForm createItem={this.createItem} />
+        )}
       </div>
 
       /* display all items */
--- src/components/EducationSectionForm.js
+++ src/components/EducationSectionForm.js
@@ -1,24 +1,68 @@
 import { Component } from "react";
 
 class EducationSectionForm extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      item: {
+        school: "",
+        subject: "",
+        date: "",
+      },
+    };
+  }
+
+  handleChange = (e) => {
+    switch (e.target.id) {
+      case "school":
+        this.setState({
+          item: {
+            school: e.target.value,
+            subject: this.state.item.subject,
+            date: this.state.item.date,
+          },
+        });
+        break;
+      case "subject":
+        this.setState({
+          item: {
+            school: this.state.item.school,
+            subject: e.target.value,
+            date: this.state.item.date,
+          },
+        });
+        break;
+      case "date":
+        this.setState({
+          item: {
+            school: this.state.item.school,
+            subject: this.state.item.subject,
+            date: e.target.value,
+          },
+        });
+        break;
+      default:
+    }
   };
 
   render() {
-    return(
-      <form>
+    const { createItem } = this.props;
+
+    return (
+      <form onSubmit={(event) => createItem(event, this.state.item)}>
         <fieldset className="education-exp ">
           <label htmlFor="school">School</label>
-          <input type="text" id="school"/>
+          <input type="text" id="school" onChange={this.handleChange} />
           <label htmlFor="subject">Course of Study</label>
-          <input type="text" id="subject"/>
+          <input type="text" id="subject" onChange={this.handleChange} />
           <label htmlFor="date">Date Completed</label>
-          <input type="date" id="date" />
+          <input type="date" id="date" onChange={this.handleChange} />
         </fieldset>
+        <button type="submit">Add Experience</button>
       </form>
-    )
+    );
   }
 }
 
-export default EducationSectionForm
+export default EducationSectionForm;
